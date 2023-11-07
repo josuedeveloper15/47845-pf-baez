019521e5ef5c48f92d0d9309686b64d523b7a190
josuebaez61@gmail.com <josuebaez61@gmail.com>
2023-11-06 22:19:29 -0300

    ADD, login tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,29 +1,40 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import {
+  TranslateLoader,
+  TranslateModule,
+  TranslateService,
+  TranslateStore,
+} from '@ngx-translate/core';
+import { HttpClient } from '@angular/common/http';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
 describe('AppComponent', () => {
-  beforeEach(() => TestBed.configureTestingModule({
-    imports: [RouterTestingModule],
-    declarations: [AppComponent]
-  }));
+  beforeEach(() =>
+    TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule,
+        HttpClientTestingModule,
+        TranslateModule.forRoot({
+          loader: {
+            provide: TranslateLoader,
+            deps: [HttpClient],
+            useFactory: (http: HttpClient) => {
+              return new TranslateHttpLoader(http);
+            },
+          },
+        }),
+      ],
+      declarations: [AppComponent],
+      providers: [],
+    })
+  );
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
-  });
-
-  it(`should have as title '47845-pf-baez'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('47845-pf-baez');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('47845-pf-baez app is running!');
   });
 });
---
+++ src/app/auth/pages/login/login.component.spec.ts
@@ -0,0 +1,47 @@
+import { TestBed } from '@angular/core/testing';
+import { LoginComponent } from './login.component';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { SharedModule } from 'src/app/shared/shared.module';
+
+describe('LoginComponent', () => {
+  let loginComponent: LoginComponent;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [LoginComponent],
+      imports: [HttpClientTestingModule, SharedModule],
+    });
+    const fixture = TestBed.createComponent(LoginComponent);
+    loginComponent = fixture.componentInstance;
+  });
+
+  it('should create login component', () => {
+    expect(loginComponent).toBeTruthy();
+  });
+
+  it('Debe marcar todos los campos del form como "touched" si este es invalido', () => {
+    loginComponent.loginForm.patchValue({
+      email: 'asndfgndskgnjdsk43534',
+      password: '',
+    });
+    loginComponent.login();
+    expect(loginComponent.emailControl.touched).toBeTrue();
+    expect(loginComponent.passwordControl.touched).toBeTrue();
+  });
+
+  it('Debe llamar el metodo login del AuthService si el formulario es valid', () => {
+    loginComponent.loginForm.patchValue({
+      email: 'fakeemail@mail.com',
+      password: '123456',
+    });
+
+    const spyOnAuthServiceLogin = spyOn(
+      (loginComponent as any).authService,
+      'login'
+    );
+
+    loginComponent.login();
+
+    expect(spyOnAuthServiceLogin).toHaveBeenCalled();
+  });
+});
